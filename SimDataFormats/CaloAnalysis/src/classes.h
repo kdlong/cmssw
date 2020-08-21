01bbb0342fb8c09bad61ebe156423b968e52e098
Kenneth Long <kdlong@hep.wisc.edu>
2020-08-21 20:53:04 +0200

    Adding broken SimDataFormats for TP to SC AssociationMap

--- SimDataFormats/CaloAnalysis/src/classes.h
+++ SimDataFormats/CaloAnalysis/src/classes.h
@@ -1,3 +1,6 @@
+#include "DataFormats/Common/interface/AssociationMap.h"
+#include "DataFormats/Common/interface/AssociationMapHelpers.h"
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingParticleFwd.h"
 #include "SimDataFormats/CaloAnalysis/interface/CaloParticle.h"
 #include "SimDataFormats/CaloAnalysis/interface/CaloParticleFwd.h"
 #include "SimDataFormats/CaloAnalysis/interface/SimCluster.h"
@@ -25,5 +28,12 @@ namespace SimDataFormats {
     CaloParticleRefVector cprv;
     CaloParticleRefProd cprp;
     CaloParticleContainer cpc;
+    edm::helpers::KeyVal<edm::RefProd<std::vector<TrackingParticle> >,
+                                               edm::RefProd<std::vector<SimCluster> > > testy;
+    //std::pair<edm::Ref<std::vector<SimCluster>,SimCluster,edm::refhelper::FindUsingAdvance<std::vector<SimCluster>,SimCluster> >,double> camarche;
+    edm::AssociationMap<edm::OneToManyWithQuality<std::vector<TrackingParticle>,std::vector<SimCluster>,double,unsigned int>> test1;
+    edm::Wrapper<edm::AssociationMap<edm::OneToManyWithQuality<std::vector<TrackingParticle>,std::vector<SimCluster>,double,unsigned int>>> test;
+    edm::Ref<std::vector<TrackingParticle>,TrackingParticle,edm::refhelper::FindUsingAdvance<std::vector<TrackingParticle>,TrackingParticle> > bleh1;
+    std::vector<std::pair<edm::Ref<std::vector<SimCluster>,SimCluster,edm::refhelper::FindUsingAdvance<std::vector<SimCluster>,SimCluster> >,double> > bleh2;
   }  // namespace CaloAnalysis
 }  // namespace SimDataFormats
