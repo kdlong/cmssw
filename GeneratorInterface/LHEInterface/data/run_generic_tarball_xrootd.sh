ce24ac67ed322e2ba59f669ed44a33d8de799fa1
Kenneth Long <kdlong@hep.wisc.edu>
2020-09-16 00:55:48 +0200

    Simplify, fix some dead variables

--- GeneratorInterface/LHEInterface/data/run_generic_tarball_xrootd.sh
+++ GeneratorInterface/LHEInterface/data/run_generic_tarball_xrootd.sh
@@ -5,7 +5,7 @@ set -e
 echo "   ______________________________________     "
 
 if [ $# -lt 1 ]; then
-    echo "%MSG-ExternalLHEProducer-subprocess ERROR in external process. At least one argument required with format root://<xrd_path>/<path>"
+    echo "%MSG-ExternalLHEProducer-subprocess ERROR in external process. The gridpack path must be passed as an argument"
 fi
 if [[ $1 != "root://"* ]]; then
     echo "%MSG-ExternalLHEProducer-subprocess ERROR in external process. Path must have format root://<xrd_path>/<path>"
@@ -13,20 +13,16 @@ if [[ $1 != "root://"* ]]; then
 fi 
 
 xrd_path=$1
-gridpack=gridpack/$(basename $xrd_path)
-
-if [ ! -d gridpack ]; then
-    mkdir gridpack
-fi
+gridpack=$(basename $xrd_path)
 
 if [ -e $gridpack ]; then
     echo "%MSG-ExternalLHEProducer-subprocess WARNING: File $gridpack already exists, it will be overwritten."
     rm $gridpack
 fi
 
-xrdcp $xrd_path gridpack
+echo "%MSG-ExternalLHEProducer-subprocess INFO: Copying gridpack $xrd_path locally using xrootd"
+xrdcp $xrd_path .
 
-echo "%MSG-ExternalLHEProducer-subprocess INFO: Acesssing $path from $xrd_redirector with xrootd"
 path=`pwd`/$gridpack
 generic_script=$(dirname ${0})/run_generic_tarball_cvmfs.sh 
 . $generic_script $path ${@:2}
