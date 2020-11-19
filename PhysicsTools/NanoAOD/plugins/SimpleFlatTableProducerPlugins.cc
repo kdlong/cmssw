964145b1333e427757497479663f76cec565926b
Kenneth Long <kdlong@hep.wisc.edu>
2020-11-19 21:50:53 +0100

    Add configs for HGCAL ML Nano ntuples

--- PhysicsTools/NanoAOD/plugins/SimpleFlatTableProducerPlugins.cc
+++ PhysicsTools/NanoAOD/plugins/SimpleFlatTableProducerPlugins.cc
@@ -3,17 +3,25 @@
 #include "DataFormats/Candidate/interface/Candidate.h"
 typedef SimpleFlatTableProducer<reco::Candidate> SimpleCandidateFlatTableProducer;
 
+#include "SimDataFormats/CaloAnalysis/interface/SimCluster.h"
+typedef SimpleFlatTableProducer<SimCluster> SimpleSimClusterFlatTableProducer;
+
+#include "SimDataFormats/CaloHit/interface/PCaloHit.h"
+typedef SimpleFlatTableProducer<PCaloHit> SimplePCaloHitFlatTableProducer;
+
+#include "SimDataFormats/Track/interface/SimTrack.h"
+typedef SimpleFlatTableProducer<SimTrack> SimpleSimTrackFlatTableProducer;
+
 #include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
 typedef EventSingletonSimpleFlatTableProducer<GenEventInfoProduct> SimpleGenEventFlatTableProducer;
 
 #include "SimDataFormats/HTXS/interface/HiggsTemplateCrossSections.h"
 typedef EventSingletonSimpleFlatTableProducer<HTXS::HiggsClassification> SimpleHTXSFlatTableProducer;
 
-#include "DataFormats/Math/interface/Point3D.h"
-typedef EventSingletonSimpleFlatTableProducer<math::XYZPointF> SimpleXYZPointFlatTableProducer;
-
 #include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_FWK_MODULE(SimpleSimTrackFlatTableProducer);
+DEFINE_FWK_MODULE(SimplePCaloHitFlatTableProducer);
+DEFINE_FWK_MODULE(SimpleSimClusterFlatTableProducer);
 DEFINE_FWK_MODULE(SimpleCandidateFlatTableProducer);
 DEFINE_FWK_MODULE(SimpleGenEventFlatTableProducer);
 DEFINE_FWK_MODULE(SimpleHTXSFlatTableProducer);
-DEFINE_FWK_MODULE(SimpleXYZPointFlatTableProducer);
---
+++ PhysicsTools/NanoAOD/python/nanoHGCML_cff.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import FWCore.ParameterSet.Config as cms
+from PhysicsTools.NanoAOD.common_cff import *
+from hgcSimHits_cff import *
+from hgcSimTracks_cff import *
+from simClusters_cff import simClusterTable
+
+nanoMetadata = cms.EDProducer("UniqueStringProducer",
+    strings = cms.PSet(
+        tag = cms.string("untagged"),
+    )
+)
+
+nanoMLSequence = cms.Sequence(nanoMetadata+simclusterTable+SimTracksTable+hgcSimHitsSequence)
