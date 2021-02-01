49d949e4f6efb30241bbc5fbbc3dda39f28a73ec
Kenneth Long <kdlong@hep.wisc.edu>
2021-02-01 11:45:48 +0100

    Fix pepr simcluster merging

--- PhysicsTools/NanoAOD/python/simClusters_cff.py
+++ PhysicsTools/NanoAOD/python/simClusters_cff.py
@@ -6,17 +6,12 @@ simClusterTable = cms.EDProducer("SimpleSimClusterFlatTableProducer",
     cut = cms.string(""),
     name = cms.string("SimCluster"),
     doc  = cms.string("SimCluster information"),
-    singleton = cms.bool(False), # the number of entries is variable
-    extension = cms.bool(False), # this is the main table for the muons
+    singleton = cms.bool(False),
+    extension = cms.bool(False), 
     variables = cms.PSet(CandVars,
         lastPos_x = Var('g4Tracks.at(0).trackerSurfacePosition().x()', 'float', precision=14, doc='track x final position'),
         lastPos_y = Var('g4Tracks.at(0).trackerSurfacePosition().y()', 'float', precision=14, doc='track y final position'),
         lastPos_z = Var('g4Tracks.at(0).trackerSurfacePosition().z()', 'float', precision=14, doc='track z final position'),
-        # NOTE: This is the cms-pepr approach, which is needed for merged simclusters
-        impactPoint_x = Var('impactPoint().x()', 'float', precision=14, doc='x position'),
-        impactPoint_y = Var('impactPoint().y()', 'float', precision=14, doc='y position'),
-        impactPoint_z = Var('impactPoint().z()', 'float', precision=14, doc='z position'),
-        impactPoint_t = Var('impactPoint().t()', 'float', precision=14, doc='Impact time'),
         impactPoint_x = Var('g4Tracks().at(0).getPositionAtBoundary().x()', 'float', precision=14, doc='x position'),
         impactPoint_y = Var('g4Tracks().at(0).getPositionAtBoundary().y()', 'float', precision=14, doc='y position'),
         impactPoint_z = Var('g4Tracks().at(0).getPositionAtBoundary().z()', 'float', precision=14, doc='z position'),
@@ -43,21 +38,4 @@ simClusterToCaloPartTable = cms.EDProducer("SimClusterToCaloParticleIndexTablePr
     docString = cms.string("Index of CaloPart containing SimCluster")
 )
 
-hgcSimTruth = cms.EDProducer("HGCTruthProducer")
-
-simClusterToMergedSCTable = cms.EDProducer("SimClusterToSimClusterIndexTableProducer",
-    cut = simClusterTable.cut,
-    src = simClusterTable.src,
-    objName = simClusterTable.name,
-    branchName = cms.string("MergedSimCluster"),
-    objMap = cms.InputTag("hgcSimTruth"),
-    docString = cms.string("Index of Merged SimCluster containing SimCluster")
-)
-
-mergedSimClusterTable = simClusterTable.clone()
-mergedSimClusterTable.src = "hgcSimTruth"
-mergedSimClusterTable.name = "MergedSimCluster"
-
 simClusterTables = cms.Sequence(simClusterTable+simClusterToCaloPartTable)
-
-mergedSimClusterTables = cms.Sequence(hgcSimTruth+mergedSimClusterTable+simClusterToMergedSCTable)
