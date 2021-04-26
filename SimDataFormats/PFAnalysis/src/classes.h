5cd8f2c135b5714b0f6fe84c3fc069fa003386f8
Kenneth Long <kdlong@hep.wisc.edu>
2021-04-26 11:28:28 +0200

    Rename PFParticle to PFTruthParticle

--- SimDataFormats/PFAnalysis/src/classes.h
+++ SimDataFormats/PFAnalysis/src/classes.h
@@ -2,7 +2,7 @@
 #include "SimDataFormats/TrackingAnalysis/interface/TrackingParticleFwd.h"
 #include "SimDataFormats/CaloAnalysis/interface/SimCluster.h"
 #include "SimDataFormats/CaloAnalysis/interface/SimClusterFwd.h"
-#include "SimDataFormats/PFAnalysis/interface/PFParticle.h"
-#include "SimDataFormats/PFAnalysis/interface/PFParticleFwd.h"
+#include "SimDataFormats/PFAnalysis/interface/PFTruthParticle.h"
+#include "SimDataFormats/PFAnalysis/interface/PFTruthParticleFwd.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 
